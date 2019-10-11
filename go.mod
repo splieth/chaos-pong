f2c18fc4ce6de76df571b1a57b1f6bc6b3924694
Philipp Normann <philipp.normann@otto.de>
2019-10-11 12:03:24 +0200

    Use tcell and fill screen with x's + exit on ESC

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/splieth/chaos-pong
 
 go 1.13
 
-require (
-	github.com/mattn/go-runewidth v0.0.4 // indirect
-	github.com/nsf/termbox-go v0.0.0-20190817171036-93860e161317
-)
+require github.com/gdamore/tcell v1.3.0
--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-	"github.com/nsf/termbox-go"
+	"fmt"
+	"github.com/gdamore/tcell"
+	"log"
+	"os"
 )
 
 func main() {
-	_ = termbox.Init()
-	termbox.SetInputMode(termbox.InputEsc)
-	_ = termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	_ = termbox.Flush()
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Println("Could not start tcell for chaos-pong.")
+		log.Printf("Cannot alloc screen, tcell.NewScreen() gave an error:\n%s", err)
+		os.Exit(1)
+	}
+	err = screen.Init()
+	if err != nil {
+		fmt.Println("Could not start tcell for chaos-pong.")
+		log.Printf("Cannot start gomatrix, screen.Init() gave an error:\n%s", err)
+		os.Exit(1)
+	}
+	screen.HideCursor()
+	screen.SetStyle(tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorOrange))
+	screen.Clear()
+	for {
+		screen.Fill('x', tcell.StyleDefault)
+		screen.Show()
+		ev := screen.PollEvent()
+		if ev != nil {
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape {
+					return
+				}
+			}
+		}
+	}
 }
