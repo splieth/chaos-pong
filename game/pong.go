492190f626fc9a55367b6207b3138b8eacb0b8c7
Matthias Splieth <matthias@splieth.com>
2019-10-11 19:10:34 +0200

    Add Sprite as type to abstract vector usage

--- game/pong.go
+++ game/pong.go
@@ -6,43 +6,43 @@ import (
 	"time"
 )
 
-const FPS = 2
-const GAME_HEIGHT = 32
-const GAME_WIDTH = 64
+const (
+	FPS        = 2
+	GameHeight = 32
+	GameWidth  = 64
+)
 
-type Coordinate struct {
-	x int
-	y int
+type Vector struct {
+	x, y int
 }
 
-type Direction struct {
-	xStep int
-	yStep int
+type Sprite struct {
+	position  Vector
+	direction Vector
 }
 
 type Ball struct {
-	position Coordinate
-	dir      Direction
-	speed    int
+	sprite Sprite
+	speed  int
 }
 
-func (ball *Ball) getNextPos() Coordinate {
-	newX := ball.position.x + ball.dir.xStep*ball.speed
-	newY := ball.position.y + ball.dir.yStep*ball.speed
-	return Coordinate{newX, newY}
+func (ball *Ball) getNextPos() Vector {
+	newX := ball.sprite.position.x + ball.sprite.direction.x*ball.speed
+	newY := ball.sprite.position.y + ball.sprite.direction.y*ball.speed
+	return Vector{newX, newY}
 }
 
 func (ball *Ball) move() {
-	ball.position = ball.getNextPos()
+	ball.sprite.position = ball.getNextPos()
 }
 
-func (ball *Ball) handleCollision(topCorner Coordinate, bottomCorner Coordinate) {
+func (ball *Ball) handleCollision(topCorner Vector, bottomCorner Vector) {
 	newPos := ball.getNextPos()
 	if newPos.x <= topCorner.x || newPos.x >= bottomCorner.x {
-		ball.dir.xStep = ball.dir.xStep * -1
+		ball.sprite.direction.x = ball.sprite.direction.x * -1
 	}
 	if newPos.y <= topCorner.y || newPos.y >= bottomCorner.y {
-		ball.dir.yStep = ball.dir.yStep * -1
+		ball.sprite.direction.y = ball.sprite.direction.y * -1
 	}
 }
 
@@ -62,7 +62,7 @@ func handleKeyPresses(screen tcell.Screen) (bool, tcell.Key) {
 			switch ev := ev.(type) {
 			case *tcell.EventKey:
 				switch ev.Key() {
-				case tcell.KeyESC:
+				case tcell.KeyCtrlQ:
 					os.Exit(0)
 				}
 			}
@@ -70,35 +70,41 @@ func handleKeyPresses(screen tcell.Screen) (bool, tcell.Key) {
 	}
 }
 
-func drawSquare(screen tcell.Screen, coord Coordinate, color tcell.Color) {
+func drawSquare(screen tcell.Screen, coord Vector, color tcell.Color) {
 	screen.SetCell(coord.x, coord.y, tcell.StyleDefault.Background(color), ' ')
 	screen.SetCell(coord.x+1, coord.y, tcell.StyleDefault.Background(color), ' ')
 }
 
-func drawRectangle(screen tcell.Screen, topCorner Coordinate, bootomCorner Coordinate) {
+func drawRectangle(screen tcell.Screen, topCorner Vector, bootomCorner Vector) {
 	for x := topCorner.x; x <= bootomCorner.x; x++ {
-		drawSquare(screen, Coordinate{x, topCorner.y}, tcell.ColorWhite)
-		drawSquare(screen, Coordinate{x, bootomCorner.y}, tcell.ColorWhite)
+		drawSquare(screen, Vector{x, topCorner.y}, tcell.ColorWhite)
+		drawSquare(screen, Vector{x, bootomCorner.y}, tcell.ColorWhite)
 	}
 	for y := topCorner.y; y <= bootomCorner.y; y++ {
-		drawSquare(screen, Coordinate{topCorner.x, y}, tcell.ColorWhite)
-		drawSquare(screen, Coordinate{bootomCorner.x, y}, tcell.ColorWhite)
+		drawSquare(screen, Vector{topCorner.x, y}, tcell.ColorWhite)
+		drawSquare(screen, Vector{bootomCorner.x, y}, tcell.ColorWhite)
 	}
 }
 
-func draw(screen tcell.Screen, topCorner Coordinate, bottomCorner Coordinate, ball Ball) {
+func draw(screen tcell.Screen, topCorner Vector, bottomCorner Vector, ball Ball) {
 	screen.Clear()
 	drawRectangle(screen, topCorner, bottomCorner)
-	screen.SetCell(ball.position.x, ball.position.y, tcell.StyleDefault, '●')
+	screen.SetCell(ball.sprite.position.x, ball.sprite.position.y, tcell.StyleDefault, '●')
 	screen.Show()
 }
 
 func loop(screen tcell.Screen) {
 	go handleKeyPresses(screen)
 	termWidth, termHeight := screen.Size()
-	topCorner := Coordinate{termWidth/2 - GAME_WIDTH, termHeight/2 - GAME_HEIGHT/2}
-	bottomCorner := Coordinate{termWidth/2 + GAME_WIDTH, termHeight/2 + GAME_HEIGHT/2}
-	ball := Ball{Coordinate{termWidth / 2, termHeight / 2}, Direction{1, 1}, 1}
+	topCorner := Vector{termWidth/2 - GameWidth, termHeight/2 - GameHeight/2}
+	bottomCorner := Vector{termWidth/2 + GameWidth, termHeight/2 + GameHeight/2}
+	ball := Ball{
+		sprite: Sprite{
+			position:  Vector{termWidth / 2, termHeight / 2},
+			direction: Vector{x: 1, y: 1,},
+		},
+		speed: 1,
+	}
 	for {
 		ball.handleCollision(topCorner, bottomCorner)
 		ball.move()
