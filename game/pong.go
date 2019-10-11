7e3e3bf1337d3e054a2320209044b618d2b0e104
Matthias Splieth <matthias@splieth.com>
2019-10-11 20:41:48 +0200

    Introduce canvas type in order to be able to draw multiple canvases (e.g. for the points of each player)

--- game/pong.go
+++ game/pong.go
@@ -7,9 +7,8 @@ import (
 )
 
 const (
-	FPS        = 2
-	GameHeight = 32
-	GameWidth  = 64
+	FPS           = 3
+	CanvasPadding = 4
 )
 
 type Vector struct {
@@ -26,23 +25,18 @@ type Ball struct {
 	speed  int
 }
 
-func (ball *Ball) getNextPos() Vector {
-	newX := ball.sprite.position.x + ball.sprite.direction.x*ball.speed
-	newY := ball.sprite.position.y + ball.sprite.direction.y*ball.speed
-	return Vector{newX, newY}
+type Canvas struct {
+	x      int
+	y      int
+	width  int
+	height int
 }
 
-func (ball *Ball) move() {
-	ball.sprite.position = ball.getNextPos()
-}
-
-func (ball *Ball) handleCollision(topCorner Vector, bottomCorner Vector) {
-	newPos := ball.getNextPos()
-	if newPos.x <= topCorner.x || newPos.x >= bottomCorner.x {
-		ball.sprite.direction.x = ball.sprite.direction.x * -1
-	}
-	if newPos.y <= topCorner.y || newPos.y >= bottomCorner.y {
-		ball.sprite.direction.y = ball.sprite.direction.y * -1
+func (c *Canvas) draw(screen tcell.Screen) {
+	for col := c.x; col < c.width; col++ {
+		for row := c.y; row < c.height; row++ {
+			screen.SetContent(col, row, ' ', nil, tcell.StyleDefault.Background(tcell.ColorRebeccaPurple))
+		}
 	}
 }
 
@@ -53,6 +47,29 @@ func Start(screen tcell.Screen) {
 		Foreground(tcell.ColorOrange))
 	screen.Clear()
 	loop(screen)
+}
+
+func (ball *Ball) getNextPos() Vector {
+	newX := ball.sprite.position.x + ball.sprite.direction.x*ball.speed
+	newY := ball.sprite.position.y + ball.sprite.direction.y*ball.speed
+	return Vector{newX, newY}
+}
+
+func (ball *Ball) move(screen tcell.Screen) {
+	ball.sprite.position = ball.getNextPos()
+	screen.SetContent(ball.sprite.position.x, ball.sprite.position.y, '●', nil, tcell.StyleDefault.Background(tcell.ColorRebeccaPurple).Foreground(tcell.ColorOrangeRed))
+}
+
+// FIXME border detection is worse than before
+func (ball *Ball) handleCollision(c *Canvas) {
+	newPos := ball.getNextPos()
+
+	if newPos.x > c.x+c.width-CanvasPadding-1 || newPos.x <= c.x {
+		ball.sprite.direction.x = ball.sprite.direction.x * -1
+	}
+	if newPos.y < c.y || newPos.y >= c.y+c.height-CanvasPadding {
+		ball.sprite.direction.y = ball.sprite.direction.y * -1
+	}
 }
 
 func handleKeyPresses(screen tcell.Screen) (bool, tcell.Key) {
@@ -70,45 +87,27 @@ func handleKeyPresses(screen tcell.Screen) (bool, tcell.Key) {
 	}
 }
 
-func drawSquare(screen tcell.Screen, coord Vector, color tcell.Color) {
-	screen.SetCell(coord.x, coord.y, tcell.StyleDefault.Background(color), ' ')
-	screen.SetCell(coord.x+1, coord.y, tcell.StyleDefault.Background(color), ' ')
-}
-
-func drawRectangle(screen tcell.Screen, topCorner Vector, bootomCorner Vector) {
-	for x := topCorner.x; x <= bootomCorner.x; x++ {
-		drawSquare(screen, Vector{x, topCorner.y}, tcell.ColorWhite)
-		drawSquare(screen, Vector{x, bootomCorner.y}, tcell.ColorWhite)
-	}
-	for y := topCorner.y; y <= bootomCorner.y; y++ {
-		drawSquare(screen, Vector{topCorner.x, y}, tcell.ColorWhite)
-		drawSquare(screen, Vector{bootomCorner.x, y}, tcell.ColorWhite)
-	}
-}
-
-func draw(screen tcell.Screen, topCorner Vector, bottomCorner Vector, ball Ball) {
-	screen.Clear()
-	drawRectangle(screen, topCorner, bottomCorner)
-	screen.SetCell(ball.sprite.position.x, ball.sprite.position.y, tcell.StyleDefault, '●')
-	screen.Show()
-}
-
 func loop(screen tcell.Screen) {
 	go handleKeyPresses(screen)
 	termWidth, termHeight := screen.Size()
-	topCorner := Vector{termWidth/2 - GameWidth, termHeight/2 - GameHeight/2}
-	bottomCorner := Vector{termWidth/2 + GameWidth, termHeight/2 + GameHeight/2}
+	ballCanvas := Canvas{
+		x:      CanvasPadding,
+		y:      CanvasPadding,
+		width:  termWidth - CanvasPadding,
+		height: termHeight - CanvasPadding,
+	}
 	ball := Ball{
 		sprite: Sprite{
-			position:  Vector{termWidth / 2, termHeight / 2},
+			position:  Vector{(ballCanvas.width / 2) + CanvasPadding, (ballCanvas.height / 2) + CanvasPadding},
 			direction: Vector{x: 1, y: 1,},
 		},
 		speed: 1,
 	}
 	for {
-		ball.handleCollision(topCorner, bottomCorner)
-		ball.move()
-		draw(screen, topCorner, bottomCorner, ball)
+		ball.handleCollision(&ballCanvas)
+		ballCanvas.draw(screen)
+		ball.move(screen)
+		screen.Show()
 		duration := (100 / FPS) * time.Millisecond
 		time.Sleep(duration)
 	}
