aa9f789dfdccd87e3464468f9b745132fa1e46fa
Philipp Normann <philipp.normann@otto.de>
2019-10-17 13:38:50 +0200

    Use float vector for ball direction

--- game/vector.go
+++ game/vector.go
@@ -1,25 +1,49 @@
 package game
 
-type Vector struct {
+import "math"
+
+type FloatVector struct {
+	x, y float64
+}
+
+type IntVector struct {
 	x, y int
 }
 
-func Add(v1 Vector, v2 Vector) Vector {
-	return Vector{v1.x + v2.x, v1.y + v2.y}
+func (v FloatVector) convertToInt() IntVector {
+	return IntVector{
+		x: int(math.Round(v.x)),
+		y: int(math.Round(v.y)),
+	}
 }
 
-func Up() Vector {
-	return Vector{0, -1}
+func (v IntVector) convertToFloat() FloatVector {
+	return FloatVector{
+		x: float64(v.x),
+		y: float64(v.y),
+	}
 }
 
-func Right() Vector {
-	return Vector{-1, 0}
+func (v IntVector) Add(other IntVector) IntVector {
+	return IntVector{v.x + other.x, v.y + other.y}
 }
 
-func Down() Vector {
-	return Vector{0, 1}
+func (v FloatVector) Add(other FloatVector) FloatVector {
+	return FloatVector{v.x + other.x, v.y + other.y}
 }
 
-func Left() Vector {
-	return Vector{1, 0}
+func Up() IntVector {
+	return IntVector{0, -1}
+}
+
+func Right() IntVector {
+	return IntVector{-1, 0}
+}
+
+func Down() IntVector {
+	return IntVector{0, 1}
+}
+
+func Left() IntVector {
+	return IntVector{1, 0}
 }
