ceaf587ec191a34509fa22dc44bed16787b4a25e
Matthias Splieth <matthias@splieth.com>
2019-10-18 20:33:13 +0200

    Fix collision with wall (make @philippnormann1337 happy)

--- game/types/ball.go
+++ game/types/ball.go
@@ -27,13 +27,13 @@ func (ball *Ball) Move(screen *ebiten.Image) {
 	if ball.pos.X < 0 {
 		ball.vel.X = -ball.vel.X
 	}
-	if ball.pos.X > float64(screenWidth-ball.Radius) {
+	if ball.pos.X > float64(screenWidth-2*ball.Radius) {
 		ball.vel.X = -ball.vel.X
 	}
 	if ball.pos.Y < 0 {
 		ball.vel.Y = -ball.vel.Y
 	}
-	if ball.pos.Y > float64(screenHeight-ball.Radius) {
+	if ball.pos.Y > float64(screenHeight-2*ball.Radius) {
 		ball.vel.Y = -ball.vel.Y
 	}
 
