208901d6f6381ed588cdd15ddc0e1678b80cdf7e
Philipp Normann <philipp.normann@otto.de>
2019-10-28 17:38:19 +0100

    Implement super fancy line intersection
    Co-authored-by: Matthias Splieth <matthias@splieth.com>

--- game/types/vector.go
+++ game/types/vector.go
@@ -1,12 +1,17 @@
 package types
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 )
 
 type Vector struct {
 	X, Y float64
+}
+
+type Line struct {
+	Start, End Vector
 }
 
 func (v *Vector) Add(other Vector) {
@@ -45,4 +50,25 @@ func (v *Vector) Randomize() {
 		randomAddition = float64(rand.Intn(1000)) / 1000
 	}
 	v.Y += randomAddition
+}
+
+func det(a, b Vector) float64 {
+	return a.X*b.Y - a.Y*b.X
+}
+
+func (l *Line) diff() (float64, float64) {
+	return l.Start.X - l.End.X, l.Start.Y - l.End.Y
+}
+
+func Intersect(line1, line2 Line) (Vector, error) {
+	xDiff1, yDiff1 := line1.diff()
+	xDiff2, yDiff2 := line2.diff()
+	xDiff := Vector{xDiff1, xDiff2}
+	yDiff := Vector{yDiff1, yDiff2}
+	div := det(xDiff, yDiff)
+	if div == 0 {
+		return Vector{}, fmt.Errorf("No intersection")
+	}
+	d := Vector{det(line1.Start, line1.End), det(line2.Start, line2.End)}
+	return Vector{det(d, xDiff) / div, det(d, yDiff) / div}, nil
 }
---
+++ game/types/vector_test.go
@@ -0,0 +1,41 @@
+package types
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var intersectTests = []struct {
+	name         string
+	line1        Line
+	line2        Line
+	intersection Vector
+	err          error
+}{
+	{"Lines intersect",
+		Line{Start: Vector{0, 0}, End: Vector{10, 10}},
+		Line{Start: Vector{0, 10}, End: Vector{10, 0}},
+		Vector{X: 5, Y: 5},
+		nil},
+	{"Lines intersect in inverse direction",
+		Line{Start: Vector{20, 20}, End: Vector{0, 0}},
+		Line{Start: Vector{0, 20}, End: Vector{20, 0}},
+		Vector{X: 10, Y: 10},
+		nil},
+	{"No intersection",
+		Line{Start: Vector{0, 0}, End: Vector{0, 1}},
+		Line{Start: Vector{1, 0}, End: Vector{1, 1}},
+		Vector{},
+		fmt.Errorf("No intersection")},
+}
+
+func TestResponses(t *testing.T) {
+	for _, test := range intersectTests {
+		t.Run(test.name, func(t *testing.T) {
+			v, e := Intersect(test.line1, test.line2)
+			assert.Equal(t, test.err, e)
+			assert.Equal(t, test.intersection, v)
+		})
+	}
+}
